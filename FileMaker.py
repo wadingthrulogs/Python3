e85f8d564c567a1bce4e5954c5fa8239f2041965
wadingthrulogs <51767442+wadingthrulogs@users.noreply.github.com>
2019-11-26 14:26:55 -0800

    Update and rename 123.py to FileMaker.py

--- FileMaker.py
+++ FileMaker.py
@@ -1,15 +1,16 @@
+#Makes files
 import os
 import uuid
 import random
 count = 0
 
-
-
 while count < 500:
+    #file size Range in KB
     r1 = random.randrange(1048576,2097152)
+    #Random file name
     filename = str(uuid.uuid4())
     with open(filename, 'wb') as fout:
-        fout.write(os.urandom(r1))  # replace 1024 with size_kb if not unreasonably large
+        fout.write(os.urandom(r1))
     count = count + 1
 else:
     print('files made')
